08279bcabfb71abdd1de268372a34d6cdbe47c96
folaoluwafemi <folaoluwafemi55@gmail.com>
2022-04-01 00:02:46 +0100

    clean up code

--- app/src/main/java/com/popestrings/notificationapp/MainActivity.kt
+++ app/src/main/java/com/popestrings/notificationapp/MainActivity.kt
@@ -6,18 +6,23 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private val CHANNEL_ID = "channel_id_01"
+    private val channelId = "channel_id_01"
+    private val notificationId = 134
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createNotificationChannel()
-        val notifyBtn: Button = findViewById<Button>(R.id.notify_btn)
+        val notifyBtn: Button = findViewById(R.id.notify_btn)
+
+
+
 
         notifyBtn.setOnClickListener {
             sendNotification()
@@ -29,27 +34,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "my notifications"
+                val name = "my notifications"
             val descriptionText = "My notification description"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel: NotificationChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            val channel = NotificationChannel(channelId, name, importance)
             channel.description = descriptionText
             val notificationManager: NotificationManager =
-                (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).apply {
-                    createNotificationChannel(channel)
-                }
+                (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+            notificationManager.createNotificationChannel(channel)
+
         }
     }
 
     private fun sendNotification() {
-        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.emoji_icon)
             .setContentTitle("Custom smiley notification")
             .setContentText("This is the content of the notification")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         with(NotificationManagerCompat.from(this)) {
-
-            notify(0, builder.build())
+            notify(notificationId, builder.build())
         }
     }
 
